01fbed51790575854f5782799a08fd805743da28
leandromaialeqa <leandromaiale1@gmail.com>
2026-01-31 15:11:43 -0600

    New configuration for divices

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,10 +38,6 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
       name: 'ui',
       testMatch: /.*\.ui\.spec\.ts/,
       use: {
@@ -53,15 +49,6 @@ export default defineConfig({
       testMatch: /.*\.api\.spec\.ts/,
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
     {
       name: 'API Test',
       testMatch: 'APITest/**/*',
@@ -74,26 +61,6 @@ export default defineConfig({
       }
 
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
